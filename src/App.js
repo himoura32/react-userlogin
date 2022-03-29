2b48f223f079afe5024e70de2399eae0e639e9f8
himoura <fallingout31@gmail.com>
2022-03-29 14:49:00 -0400

    taking a break

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
+import AddUser from './Components/Users/AddUsers';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <AddUser />
     </div>
   );
 }
